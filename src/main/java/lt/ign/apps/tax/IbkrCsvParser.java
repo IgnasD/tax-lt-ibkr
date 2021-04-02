9becd7623ed2e5b0cd302d529a87355e8cbac66e
Ignas Daukšas <ignasd@gmail.com>
2021-04-02 12:09:22 +0300

    add IBKR annual activity report CSV parser

---
+++ src/main/java/lt/ign/apps/tax/IbkrCsvParser.java
@@ -0,0 +1,71 @@
+package lt.ign.apps.tax;
+
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+
+public class IbkrCsvParser {
+
+	private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm:ss");
+
+	private final List<String> paths;
+
+	private List<Trade> trades;
+
+	public IbkrCsvParser(List<String> paths) {
+		this.paths = paths;
+	}
+
+	private Trade.Type parseTradeType(String str) {
+		switch (str) {
+		case "O":
+			return Trade.Type.OPEN;
+		case "C":
+			return Trade.Type.CLOSE;
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public void parse() {
+		trades = new ArrayList<>();
+		try {
+			for (String path : paths) {
+				try (CSVReader reader = new CSVReader(new FileReader(path))) {
+					String[] line;
+					while ((line = reader.readNext()) != null) {
+						if (line.length != 17) continue;
+						if (!line[0].equals("Trades")) continue;
+						if (!line[1].equals("Data")) continue;
+						if (!line[2].equals("Order")) continue;
+						if (!line[3].equals("Stocks")) continue;
+
+						var trade = new Trade();
+						trade.setCurrency(Currency.valueOf(line[4]));
+						trade.setSymbol(line[5]);
+						trade.setDateTime(LocalDateTime.parse(line[6], dateTimeFormatter));
+						trade.setQuantity(Integer.parseInt(line[7]));
+						trade.setProceeds(new BigDecimal(line[10]));
+						trade.setFees(new BigDecimal(line[11]));
+						trade.setType(parseTradeType(line[16]));
+						trades.add(trade);
+
+						// TODO adjust for stock splits
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<Trade> getTrades() {
+		return trades;
+	}
+
+}
---
+++ src/main/java/lt/ign/apps/tax/Trade.java
@@ -0,0 +1,76 @@
+package lt.ign.apps.tax;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Trade {
+
+	private Type type;
+	private String symbol;
+	private LocalDateTime dateTime;
+	private int quantity;
+	private BigDecimal proceeds;
+	private BigDecimal fees;
+	private Currency currency;
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getProceeds() {
+		return proceeds;
+	}
+
+	public void setProceeds(BigDecimal proceeds) {
+		this.proceeds = proceeds;
+	}
+
+	public BigDecimal getFees() {
+		return fees;
+	}
+
+	public void setFees(BigDecimal fees) {
+		this.fees = fees;
+	}
+
+	public Currency getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+
+	public enum Type {
+		OPEN, CLOSE
+	}
+
+}
